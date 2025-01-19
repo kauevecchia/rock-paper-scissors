32c6a7af33e519606aae6d210c871c8d88e57022
Kauê <kauevecchia@hotmail.com>
2025-01-19 00:05:23 -0300

    function updateResult pronta

---
+++ src/js/ui.js
@@ -0,0 +1,29 @@
+import { gameState } from "./game.js";
+
+
+const resultText = document.getElementById("result")
+const playerScore = document.getElementById("playerScore")
+const machineScore = document.getElementById("machineScore")
+const resultDisplay = document.getElementById("resultDisplay")
+const optionsDisplay = document.getElementById("optionsContainer")
+const rulesModal = document.getElementById("rulesModal")
+const machineChoiceImg = document.getElementById("machineChoice")
+const playerChoiceImg = document.getElementById("playerChoice")
+
+
+export function updateResult() {
+    resultText.classList.remove("text-green-600", "text-red-600")
+    
+    playerChoiceImg.src = `assets/${gameState.playerChoice}.svg`
+    machineChoiceImg.src = `assets/${gameState.machineChoice}.svg`
+
+    if (gameState.result === "PLAYER WINS") {
+        resultText.textContent = gameState.result
+        resultText.classList.add("text-green-600")
+    } else if (gameState.result === "PLAYER LOSES") {
+        resultText.textContent = gameState.result
+        resultText.classList.add("text-red-600")
+    } else {
+        resultText.textContent = gameState.result
+    }
+}
