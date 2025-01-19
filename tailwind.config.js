55eaeabee42808a559121d9a9d2e1347098ea098
Kauê <kauevecchia@hotmail.com>
2025-01-19 01:24:13 -0300

    animações adicionadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'], 
       },
+
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-50%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        showUp: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        slideDown: 'slideDown .8s ease-out',
+        showUp: 'showUp .8s ease-in-out',
+      },
     },
   },
   plugins: [],
